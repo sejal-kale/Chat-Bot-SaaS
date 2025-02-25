e444dc2f2b88bf08203c9160beb544c4c9a823cc
sejal-kale <sejalk1910@gmail.com>
2025-02-25 22:32:46 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
-);
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+connectToDatabase().then(()=>{
+  
+  app.listen(5000,()=>{
+    console.log("I am listening on the port : ",5000)
+  })
+  
+})
